0ec62f98b703d3f6681c90b3022be2e65259f7a6
Joaquin Sopena <696272@unizar.es>
2021-12-12 14:17:37 +0100

    Task from WIND_IOT_ESP1 send_sensor_data_ble starts a ble service and sends characteristics which data is related to the three axis sensor

--- WIND_IOT_ESP1/WIND_IOT_ESP1.ino
+++ WIND_IOT_ESP1/WIND_IOT_ESP1.ino
@@ -1,12 +1,115 @@
 #include <MPU9250_asukiaaa.h>
 #include <Adafruit_BMP280.h>
 #include <string.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+#include <stdio.h>
 
+//----------------------------------------------------
+//GLOBAL VARIABLES
+
+//SENSOR
 struct Three_axis_sensor_pressure {
   float aX, aY, aZ, gX, gY, gZ, mDirection, mX, mY, mZ, temp, pressure;
 };
-
 QueueHandle_t SensorQueueS;
+
+//BLE
+#define SERVICE_UUID        BLEUUID((uint16_t)0x181A)   //Enviromental Sensing
+#define CHAR_READ_UUID      BLEUUID((uint16_t)0x2713)   //Acceleration
+
+// --------
+// Global variables
+// --------
+static BLEServer* g_pServer = nullptr;
+static BLECharacteristic* g_pCharRead = nullptr;
+static bool g_centralConnected = false;
+static std::string g_cmdLine;
+//SENSOR SENDED VARIABLES
+char acc_X[10];
+uint16_t value = 0;
+
+// --------
+// Bluetooth event callbacks
+// --------
+class MyServerCallbacks: public BLEServerCallbacks
+{
+    void onConnect(BLEServer* pServer) override
+    {
+      Serial.println("onConnect");
+      g_centralConnected = true;
+    }
+
+    void onDisconnect(BLEServer* pServer) override
+    {
+      Serial.println("onDisconnect, will start advertising");
+      g_centralConnected = false;
+      BLEDevice::startAdvertising();
+    }
+};
+
+class MyCharPrintingCallbacks: public BLECharacteristicCallbacks
+{
+  public:
+    explicit MyCharPrintingCallbacks(const char* name) : m_name(name) {}
+
+  private:
+    void PrintEvent(const char* event, const char* value)
+    {
+      Serial.print(event);
+      Serial.print("(");
+      Serial.print(m_name.c_str());
+      Serial.print(")");
+      if (value)
+      {
+        Serial.print(" value='");
+        Serial.print(value);
+        Serial.print("'");
+      }
+      Serial.println();
+    }
+
+  private:
+    void onRead(BLECharacteristic* pCharacteristic) override
+    {
+      PrintEvent("onRead", pCharacteristic->getValue().c_str());
+    }
+
+    void onWrite(BLECharacteristic* pCharacteristic) override
+    {
+      PrintEvent("onWrite", pCharacteristic->getValue().c_str());
+    }
+
+    void onNotify(BLECharacteristic* pCharacteristic) override
+    {
+      PrintEvent("onNotify", pCharacteristic->getValue().c_str());
+    }
+
+    void onStatus(BLECharacteristic* pCharacteristic, Status status, uint32_t code) override
+    {
+      std::string event("onStatus:");
+      switch (status)
+      {
+        case SUCCESS_INDICATE: event += "SUCCESS_INDICATE"; break;
+        case SUCCESS_NOTIFY: event += "SUCCESS_NOTIFY"; break;
+        case ERROR_INDICATE_DISABLED: event += "ERROR_INDICATE_DISABLED"; break;
+        case ERROR_NOTIFY_DISABLED: event += "ERROR_NOTIFY_DISABLED"; break;
+        case ERROR_GATT: event += "ERROR_GATT"; break;
+        case ERROR_NO_CLIENT: event += "ERROR_NO_CLIENT"; break;
+        case ERROR_INDICATE_TIMEOUT: event += "ERROR_INDICATE_TIMEOUT"; break;
+        case ERROR_INDICATE_FAILURE: event += "ERROR_INDICATE_FAILURE"; break;
+      }
+      event += ":";
+      event += String(code).c_str();
+      PrintEvent(event.c_str(), nullptr);
+    }
+
+  private:
+    std::string m_name;
+};
+//----------------------------------------------------
 
 void capture_three_axis( void *pvParameters) {
   //Variables
@@ -80,16 +183,42 @@ void send_sensor_data_ble( void *pvParameters) {
   const TickType_t xDelay1s = pdMS_TO_TICKS (1000);
   TickType_t xLastWakeTime;
 
+   //BLE Peripheral
+  BLEDevice::init("ESP32-JSA");
+  g_pServer = BLEDevice::createServer();
+  g_pServer->setCallbacks(new MyServerCallbacks());    //onConnect, onDisconnect
+  BLEService* pService = g_pServer->createService(SERVICE_UUID);
+
+    // characteristic for read //HERE SENSOR DATA CHARACTERISITCS SHOULD BE ADDED
+  {
+    uint32_t propertyFlags = BLECharacteristic::PROPERTY_READ;
+    BLECharacteristic* pCharRead = pService->createCharacteristic(CHAR_READ_UUID, propertyFlags);
+    pCharRead->setCallbacks(new MyCharPrintingCallbacks("CharRead"));
+    g_pCharRead = pCharRead;
+  }
+  
+  pService->start();
+  BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  // this fixes iPhone connection issue (don't know how it works)
+  {
+    pAdvertising->setMinPreferred(0x06);
+    pAdvertising->setMinPreferred(0x12);
+  }
+  BLEDevice::startAdvertising();
+
+  Serial.println("BLE Peripheral setup done, advertising");
+
   while (1) {
     if ( uxQueueSpacesAvailable != 0) {
       while ((xQueueReceive(SensorQueueS, (void *) &TASP_R, 0) == pdPASS)) {
       }
     }
-    Serial.println(TASP_R.aX);
+    snprintf(acc_X, sizeof(acc_X), "%f", TASP_R.aX);
+    g_pCharRead->setValue(acc_X);
     vTaskDelayUntil(&xLastWakeTime, xDelay1s);
   }
-
-
 }
 
 
